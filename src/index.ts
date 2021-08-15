af367ec2e703f4b61273f0d72cf0358e6ab3965c
AbrialWong <abrial-wong@live.com.sg>
2021-08-15 15:44:46 +0800

    complete-graphql-setup

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String) // what you want the resolver to return
+  async helloWorld() {
+    // its the same name as the query
+    return "Hello World!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
